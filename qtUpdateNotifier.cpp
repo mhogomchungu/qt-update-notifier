09948dd528172060184653d3fa2e05afba4c46ed
mhogo mchungu <mhogomchungu@gmail.com>
2013-03-17 13:04:00 -0400

    code clean up

--- qtUpdateNotifier.cpp
+++ qtUpdateNotifier.cpp
@@ -339,14 +339,14 @@ void qtUpdateNotifier::updatesFound( int st,QStringList list )
 		this->logActivity( QString( "check skipped,user is not connected to the internet" ) ) ;
 		this->showToolTip( icon,QString( "no updates foung" ) );
 	}else{
+		/*
+		 * currently,we dont get here,added for completeness' sake 
+		 */
 		icon = QString( "qt-update-notifier" ) ;
 		this->changeIcon( icon );
 		this->setStatus( KStatusNotifierItem::Passive );
 		this->logActivity( QString( "update check complete,repository is in an unknown state" ) ) ;
 		this->showToolTip( icon,QString( "no updates foung" ) );
-		/*
-		 * currently,we dont get here
-		 */
 		;
 	}
 	this->writeUpdateTimeToConfigFile() ;
