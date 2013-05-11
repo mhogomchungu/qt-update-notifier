5c018df8fa2eba71e6c955b1112946d2622fc443
mhogo mchungu <mhogomchungu@gmail.com>
2013-05-11 17:43:51 -0400

    update to version 1.3.8

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ int main( int argc,char * argv[] )
 {
 
 	KAboutData aboutData( "qt-update-notifier",0,ki18n("qt-update-notifier"),
-			      "1.3.7",ki18n("a qt based apt-get package updates checker."),
+			      "1.3.8",ki18n("a qt based apt-get package updates checker."),
 				KAboutData::License_GPL_V2,ki18n("mhogomchungu@gmail.com"),
 				KLocalizedString());
 	KCmdLineArgs::init( argc,argv,&aboutData );
